176e2f8b3fde0d1f37b79ae87f6d370296ed020c
Debasis Panda <debcetb06@gmail.com>
2019-05-11 03:01:20 -0700

    Modified the package name from eventstracker to repoeventstracker

--- src/main/java/com/debasis/repoeventstracker/RepoEventsTrackerApplication.java
+++ src/main/java/com/debasis/repoeventstracker/RepoEventsTrackerApplication.java
@@ -1,5 +1,7 @@
-package com.debasis.repoeventstracker;
+package com.debasis.eventstracker;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RepoEventsTrackerApplication {
 
 	public static void main(String[] args) {
+		final Logger LOGGER = LogManager.getLogger(RepoEventsTrackerApplication.class);
 		SpringApplication.run(RepoEventsTrackerApplication.class, args);
+		LOGGER.info("RepoEventsTrackerApplication Started Successfuly...");
 	}
 
 }
